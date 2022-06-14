0d06e61b5494b264508b6db7b00319bc268652d5
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-06-14 11:17:58 -0600

    commit tree 07538550c02e445930ad261e0a7628aeec48c98c

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
+
+  # def list_roles
+  #   list_shows = self.characters.map do |character|
+  #     this_show = Show.find(character.show_id).name
+  #     "#{character.name} - #{this_show}"
+  #   end
+  # end
+  def list_roles
+    characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors.map(&:full_name)
+      #full_name is our custom method from Actor class
+    
+  end
+
 end
